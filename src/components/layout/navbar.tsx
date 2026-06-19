e27d44a296215f80b1d65ba796ef3153a92d21e4
shuban2007 <shuban1227@gmail.com>
2026-06-20 04:53:29 +0530

    style: shift top header logo to the left

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -20,7 +20,7 @@ export function Navbar({ className }: NavbarProps) {
         className
       )}
     >
-      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 w-full items-center justify-between px-4 sm:px-6 lg:px-8">
         {/* Logo */}
         <Link href="/dashboard" className="flex items-center gap-2">
           <img 
