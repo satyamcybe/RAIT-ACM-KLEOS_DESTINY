7836d52067b23070be243414f317fc6fa1b4b14a
shuban2007 <shuban1227@gmail.com>
2026-06-20 05:38:02 +0530

    style: remove Reset Demo button from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { MockDataProvider } from "@/lib/context/MockDataContext";
-import ResetDemoButton from "@/components/ResetDemoButton";
+
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className="min-h-full bg-white font-sans text-[#111827]">
         <MockDataProvider>
           {children}
-          <ResetDemoButton />
+
         </MockDataProvider>
       </body>
     </html>
